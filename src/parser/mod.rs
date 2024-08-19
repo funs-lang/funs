e6053fc5cfea81a09cb674beefa4a3cb8d339f23
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-08-20 00:26:37 +0200

    lex: remove `ws` and `\t` as tokens
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -213,3 +213,146 @@ impl Iterator for Parser<Lexer> {
         self.parse_block()
     }
 }
+
+#[cfg(test)]
+pub mod tests {
+    use crate::{
+        lexer::Lexer, parser::Parser, source::Source, utils::file_handler::collect_fs_files,
+    };
+    use pretty_assertions::assert_eq;
+    use tracing::info;
+
+    #[test]
+    fn native_types() {
+        let fs_files = collect_fs_files("./testdata/native_types", true);
+        assert_eq!(fs_files.len(), 16);
+
+        let fs_files = fs_files.iter().filter(|p| p.ends_with("id_int_assign.fs"));
+
+        for path in fs_files {
+            info!("file -> {:?}", path);
+            eprintln!("file -> {:?}", path);
+            let input = std::fs::File::open(path.clone()).unwrap();
+            let content = std::io::read_to_string(input).unwrap();
+            let source = Source::from(content);
+
+            let fs_file = path.to_str().unwrap();
+            let output_ast = Parser::new(Lexer::new(&source)).parse();
+            let ast_file = fs_file.to_string().replace(".fs", ".ast.json");
+            let ast = std::fs::File::open(ast_file).unwrap();
+            // println!("{}", serde_json::to_string(&output_ast).unwrap());
+            let expected_ast = serde_json::from_reader(ast).unwrap();
+            assert_eq!(output_ast, expected_ast);
+        }
+    }
+
+    //     #[test]
+    //     fn functions() {
+    //         let fs_files = collect_fs_files("./testdata/functions", true);
+    //         assert_eq!(fs_files.len(), 9);
+    //
+    //         for path in fs_files {
+    //             info!("file -> {:?}", path);
+    //             eprintln!("file -> {:?}", path);
+    //             let input = std::fs::File::open(path.clone()).unwrap();
+    //             let content = std::io::read_to_string(input).unwrap();
+    //             let source = Source::from(content);
+    //             let lexer = Lexer::new(&source);
+    //             let output_tokens = lexer.collect::<Vec<Token>>();
+    //
+    //             let tokens_file = path.to_str().unwrap();
+    //             let tokens_file = tokens_file.to_string().replace(".fs", ".tokens.json");
+    //             let tokens = std::fs::File::open(tokens_file).unwrap();
+    //             let expected_tokens: Vec<Token> = serde_json::from_reader(tokens).unwrap();
+    //             assert_eq!(output_tokens, expected_tokens);
+    //         }
+    //     }
+    //
+    //     #[test]
+    //     fn lists() {
+    //         let fs_files = collect_fs_files("./testdata/lists", true);
+    //         assert_eq!(fs_files.len(), 3);
+    //
+    //         for path in fs_files {
+    //             info!("file -> {:?}", path);
+    //             eprintln!("file -> {:?}", path);
+    //             let input = std::fs::File::open(path.clone()).unwrap();
+    //             let content = std::io::read_to_string(input).unwrap();
+    //             let source = Source::from(content);
+    //             let lexer = Lexer::new(&source);
+    //             let output_tokens = lexer.collect::<Vec<Token>>();
+    //
+    //             let tokens_file = path.to_str().unwrap();
+    //             let tokens_file = tokens_file.to_string().replace(".fs", ".tokens.json");
+    //             let tokens = std::fs::File::open(tokens_file).unwrap();
+    //             let expected_tokens: Vec<Token> = serde_json::from_reader(tokens).unwrap();
+    //             assert_eq!(output_tokens, expected_tokens);
+    //         }
+    //     }
+    //
+    //     #[test]
+    //     fn tuples() {
+    //         let fs_files = collect_fs_files("./testdata/tuples", true);
+    //         assert_eq!(fs_files.len(), 3);
+    //
+    //         for path in fs_files {
+    //             info!("file -> {:?}", path);
+    //             eprintln!("file -> {:?}", path);
+    //             let input = std::fs::File::open(path.clone()).unwrap();
+    //             let content = std::io::read_to_string(input).unwrap();
+    //             let source = Source::from(content);
+    //             let lexer = Lexer::new(&source);
+    //             let output_tokens = lexer.collect::<Vec<Token>>();
+    //
+    //             let tokens_file = path.to_str().unwrap();
+    //             let tokens_file = tokens_file.to_string().replace(".fs", ".tokens.json");
+    //             let tokens = std::fs::File::open(tokens_file).unwrap();
+    //             let expected_tokens: Vec<Token> = serde_json::from_reader(tokens).unwrap();
+    //             assert_eq!(output_tokens, expected_tokens);
+    //         }
+    //     }
+    //
+    //     #[test]
+    //     fn records() {
+    //         let fs_files = collect_fs_files("./testdata/records", true);
+    //         assert_eq!(fs_files.len(), 3);
+    //
+    //         for path in fs_files {
+    //             info!("file -> {:?}", path);
+    //             eprintln!("file -> {:?}", path);
+    //             let input = std::fs::File::open(path.clone()).unwrap();
+    //             let content = std::io::read_to_string(input).unwrap();
+    //             let source = Source::from(content);
+    //             let lexer = Lexer::new(&source);
+    //             let output_tokens = lexer.collect::<Vec<Token>>();
+    //
+    //             let tokens_file = path.to_str().unwrap();
+    //             let tokens_file = tokens_file.to_string().replace(".fs", ".tokens.json");
+    //             let tokens = std::fs::File::open(tokens_file).unwrap();
+    //             let expected_tokens: Vec<Token> = serde_json::from_reader(tokens).unwrap();
+    //             assert_eq!(output_tokens, expected_tokens);
+    //         }
+    //     }
+    //
+    //     #[test]
+    //     fn variants() {
+    //         let fs_files = collect_fs_files("./testdata/variants", true);
+    //         assert_eq!(fs_files.len(), 1);
+    //
+    //         for path in fs_files {
+    //             info!("file -> {:?}", path);
+    //             eprintln!("file -> {:?}", path);
+    //             let input = std::fs::File::open(path.clone()).unwrap();
+    //             let content = std::io::read_to_string(input).unwrap();
+    //             let source = Source::from(content);
+    //             let lexer = Lexer::new(&source);
+    //             let output_tokens = lexer.collect::<Vec<Token>>();
+    //
+    //             let tokens_file = path.to_str().unwrap();
+    //             let tokens_file = tokens_file.to_string().replace(".fs", ".tokens.json");
+    //             let tokens = std::fs::File::open(tokens_file).unwrap();
+    //             let expected_tokens: Vec<Token> = serde_json::from_reader(tokens).unwrap();
+    //             assert_eq!(output_tokens, expected_tokens);
+    //         }
+    //     }
+}
